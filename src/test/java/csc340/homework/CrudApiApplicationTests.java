bc991c337115da0a27ff016a5f9c876d508c2960
Katie <kalufukatie@gmail.com>
2026-03-05 13:45:43 -0500

    Added in project scaffolding

---
+++ src/test/java/csc340/homework/CrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package csc340.homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
